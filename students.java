9ef85805f095ca62fbe4b2b0ed5d2fbc3d708c10
Ing Wens <phawenslouisjean9@gmail.com>
2024-03-27 14:29:28 -0400

    Creation d'une classe students

---
+++ students.java
@@ -0,0 +1,11 @@
+import java.sql.Date;
+
+public class students {
+
+    private String lastname;
+    private String firstname;
+    private String gender;
+    private String email;
+    private String password;
+    private Date DOB;
+}
